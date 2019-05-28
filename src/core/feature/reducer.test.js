89743ecb290e95dc517ea9a2ebd9c6a3b439f7cc
Quang Tran <quangta93@gmail.com>
2019-05-28 13:56:15 -0500

    reducer tests

--- src/core/feature/reducer.test.js
+++ src/core/feature/reducer.test.js
@@ -1,6 +1,20 @@
 import { reducer, initialState } from './reducer';
+import { actForStore } from './actions';
 
 
 test('reducer initializes to initialState', () => {
   expect(reducer(undefined, {})).toEqual(initialState);
+});
+
+// NOTE: This test tests behavior of this particular reducer.
+// Needs modifications if reducer function changes.
+test('reducer returns state regardless of the payload', () => {
+  let action = actForStore();
+  expect(reducer(initialState, action)).toEqual(initialState);
+
+  action = actForStore('string');
+  expect(reducer(initialState, action)).toEqual(initialState);
+
+  action = actForStore(Math.floor(Math.random() * 10000));
+  expect(reducer(initialState, action)).toEqual(initialState);
 });
