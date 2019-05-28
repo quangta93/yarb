41daeb3598a66dc31107bac220c665748673894d
Quang Tran <quangta93@gmail.com>
2019-05-28 15:09:28 -0500

    add redux devtools

--- src/core/store.js
+++ src/core/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { reducers } from './reducers';
 import { sagas } from './sagas';
 
@@ -7,10 +9,16 @@ import { sagas } from './sagas';
 export const getStore = () => {
   const sagaMiddleware = createSagaMiddleware();
 
-  const store = createStore(
-    reducers,
-    applyMiddleware(sagaMiddleware),
-  );
+  let middlewares = applyMiddleware(sagaMiddleware);
+  const devToolEnabled = (process.env.NODE_ENV === 'development');
+
+  if (devToolEnabled) {
+    // NOTE: For more options, visit https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md#windowdevtoolsextensionconfig
+    const enhancer = composeWithDevTools({});
+    middlewares = enhancer(middlewares);
+  }
+
+  const store = createStore(reducers, middlewares);
 
   sagaMiddleware.run(sagas);
 
